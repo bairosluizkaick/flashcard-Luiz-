07ba77b497de1fa286512ebb4900528ad7f561ff
Rapoza <141277901+bairosluizkaick@users.noreply.github.com>
2024-10-22 09:21:42 -0300

    ok

---
+++ perguntas.js
@@ -0,0 +1,30 @@
+criaCartao(
+    'Programação',
+    'O que é html?',
+    'È uma linguagem de marcação de HiperTexto',
+)
+criaCartao(
+    'Programação',
+    'O que é Java Script?',
+    'É uma linguagem de programação que permite colocar itens em páginas web',
+)'
+criaCartao(
+   'Programação',
+    'O que o css faz?',
+    'Permite a personalização da aparência dos elemetos html',
+)
+criaCartao(
+    'Programação',
+    'O que é body?',
+    'É o documento em si, a informação legível para o leitor do documento',
+)
+criaCartao(
+    'Programação',
+    'O que é header?',
+    'Conhecido como "cabeçalho", é a parte superior dos site, onde fica destacada do resto do conteúdo.',
+)
+criaCartao(
+    'Programação',
+    'O que é footer?',
+    'É o elemento de "fechamento" de conteúdo de página.',
+)
